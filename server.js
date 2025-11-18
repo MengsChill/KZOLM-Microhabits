b6bb2cc5665e7180b3936e6a785c143c6178eb1d
Meng <meng6828@gmail.com>
2025-11-18 22:16:19 +0800

    a

--- server.js
+++ server.js
@@ -355,9 +355,9 @@ function initializeApp() {
         res.sendFile(path.join(__dirname, "public/html/meditation.html"))
     })
 
-    app.get("/challange", (req, res) => {
+    app.get("/challenge", (req, res) => {
         if (!req.session.userId) return res.redirect("/signin")
-        res.sendFile(path.join(__dirname, "public/html/challange.html"))
+        res.sendFile(path.join(__dirname, "public/html/challenge.html"))
     })
 
     app.get("/goals", (req, res) => {
